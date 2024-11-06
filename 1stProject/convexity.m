4c87095e24f285583b65c6d3f765aadc9f3d139b
Kou-ding <fotispapadak@gmail.com>
2024-11-06 23:24:19 +0200

    finishing touches

--- 1stProject/convexity.m
+++ 1stProject/convexity.m
@@ -9,6 +9,8 @@ f3 = exp(x) * (x^3 - 1) + (x - 1) * sin(x);
 % Compute the second derivatives
 f1_second_derivative = diff(f1, x, 2);
 f2_second_derivative = diff(f2, x, 2);
+
+f3_first_derivative = diff(f3, x);
 f3_second_derivative = diff(f3, x, 2);
 
 % Display the results
@@ -17,6 +19,9 @@ disp(f1_second_derivative);
 
 disp('Second derivative of f2(x):');
 disp(f2_second_derivative);
+
+disp('First derivative of f3(x):');
+disp(f3_first_derivative);
 
 disp('Second derivative of f3(x):');
 disp(f3_second_derivative);
